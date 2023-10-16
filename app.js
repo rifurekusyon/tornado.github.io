60c3041e89d3ff2cec3fd085cb1aac30e1f34ed7
DNSりふ <108679694+rifurekusyon@users.noreply.github.com>
2023-10-16 13:04:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 40,
+        "value": 30,
         "density": {
           "enable": true,
           "value_area": 800
